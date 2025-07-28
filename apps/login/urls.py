326ee4c85febc146ccbf58e387a6cdaa0c6b3b8b
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2025-07-27 22:28:31 -0300

    feat: atualizar rota para redirecionar / para home

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 
 urlpatterns = [
-    path("", views.HomeView, name="home"),
+    path('', RedirectView.as_view(pattern_name='home', permanent=False)),
+    path('home/', views.HomeView, name='home'),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
