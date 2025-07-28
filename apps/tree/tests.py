f0d34ccdfa27e30be14d9447596f17ecf3a17c9b
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2025-07-28 09:14:57 -0300

    feat: adicionar testes para plant_trees e plant_tree

--- apps/tree/tests.py
+++ apps/tree/tests.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.utils import timezone
 from django.test import TestCase
 from django.urls import reverse
@@ -106,3 +107,57 @@ class PlantedTreeBaseTestCase(TestCase):
         self.assertContains(response, "Ipê-amarelo")
         self.assertContains(response, "Hortelã")
         self.assertNotContains(response, "Pau-brasil")
+
+
+    def test_plant_tree_creates_planted_tree_object(self):
+        initial_count = PlantedTree.objects.count()
+
+        planted_tree = self.ext_a.plant_tree(
+            tree=self.tree_b,
+            account=self.account_a,
+            age=7,
+            latitude="10.000000",
+            longitude="20.000000"
+        )
+
+        self.assertEqual(PlantedTree.objects.count(), initial_count + 1)
+        self.assertEqual(planted_tree.user, self.user_a)
+        self.assertEqual(planted_tree.tree, self.tree_b)
+        self.assertEqual(planted_tree.account, self.account_a)
+        self.assertEqual(planted_tree.age, 7)
+        self.assertEqual(planted_tree.location_latitude, Decimal("10.000000"))
+        self.assertEqual(planted_tree.location_longitude, Decimal("20.000000"))
+
+
+    def test_plant_trees_creates_multiple_planted_tree_objects(self):
+        initial_count = PlantedTree.objects.count()
+
+        tree_data_list = [
+            {
+                "tree": self.tree_a,
+                "account": self.account_a,
+                "age": 3,
+                "latitude": "11.111111",
+                "longitude": "22.222222"
+            },
+            {
+                "tree": self.tree_c,
+                "account": self.account_a,
+                "age": 6,
+                "latitude": "33.333333",
+                "longitude": "44.444444"
+            },
+        ]
+
+        planted_trees = self.ext_a.plant_trees(tree_data_list)
+
+        self.assertEqual(len(planted_trees), 2)
+        self.assertEqual(PlantedTree.objects.count(), initial_count + 2)
+
+        for idx, data in enumerate(tree_data_list):
+            self.assertEqual(planted_trees[idx].user, self.user_a)
+            self.assertEqual(planted_trees[idx].tree, data["tree"])
+            self.assertEqual(planted_trees[idx].account, data["account"])
+            self.assertEqual(planted_trees[idx].age, data["age"])
+            self.assertEqual(planted_trees[idx].location_latitude, Decimal(data["latitude"]))
+            self.assertEqual(planted_trees[idx].location_longitude, Decimal(data["longitude"]))
